72fefc7905e5f88442d06cd8c44d31cb7776ba5b
Slawa Gladkov <slawa.gladkov@invision.de>
2017-05-16 18:59:29 +0200

    Add counter to StoreController

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
+  before_action :increment_count, only: [:index]
+
   def index
     @products = Product.order(:title)
   end
+
+  private
+    def increment_count
+      session[:counter].nil? ? session[:counter] = 0 : session[:counter]+=1
+      @counter = session[:counter]
+    end
 end
