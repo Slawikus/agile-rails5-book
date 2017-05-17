a5fbbb9ad626afc837bad52809256ce2a55f66ec
Slawa Gladkov <slawa.gladkov@invision.de>
2017-05-17 08:43:50 +0200

    Empty counter after product is added to the cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -4,6 +4,8 @@ class LineItemsController < ApplicationController
   before_action :set_cart, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
+  after_action :empty_counter, only: [:create]
+
   # GET /line_items
   # GET /line_items.json
   def index
@@ -70,6 +72,10 @@ class LineItemsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_line_item
       @line_item = LineItem.find(params[:id])
+    end
+
+    def empty_counter
+      session[:counter] = nil
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
