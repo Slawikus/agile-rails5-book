31b423fc6cbe6f3b89abb6af8ad15a7894ad372f
Slawa Gladkov <slawa.gladkov@invision.de>
2017-05-17 08:30:16 +0200

    Refactor counter in StoreController

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,13 +1,14 @@
 class StoreController < ApplicationController
-  before_action :increment_count, only: [:index]
+  before_action :increment_counter, only: [:index]
 
   def index
     @products = Product.order(:title)
   end
 
   private
-    def increment_count
-      session[:counter].nil? ? session[:counter] = 0 : session[:counter]+=1
-      @counter = session[:counter]
+    def increment_counter
+      @counter = session[:counter] || 0
+      @counter += 1
+      session[:counter] = @counter
     end
 end
